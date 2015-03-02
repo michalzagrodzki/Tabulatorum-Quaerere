982f1cc7d38fd381dfe9db3385dc24abc7ab7ba8
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-02 19:16:14 +0100

    Specify index link in Stories Controller.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,14 @@
 class StoriesController < ApplicationController
+
+  before_action :fetch_stories, only: [:index ]
+
   def index
+    @stories = Story.all
+  end
+
+  private
+
+  def fetch_stories
+    @stories = Story.all
   end
 end
