64aa7e1b17b532d6933b11de0621c7ee6b6e10c3
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-03 21:13:24 +0100

    Add search method to Model of Story.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,12 @@
 class Story < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+  validates :content, presence: true
+
+  # Returns story, which contains at least one word from query
+  def self.search(query)
+    # parameters for exact search
+    where("name like ?", "%#{query}%")
+  end
+
 end
